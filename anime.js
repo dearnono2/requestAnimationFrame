4f594172032b309c0f24428bb30156faaeca4523
dearnono2 <dearnono2@gmail.com>
2022-11-10 12:21:04 +0900

    초기 수치값을 다시 퍼센트로 변환해서 적용

--- anime.js
+++ anime.js
@@ -4,45 +4,39 @@ const box = document.querySelector('#box');
 btn.addEventListener('click', () => {
   anime(box, {
     prop: 'margin-left',
-    value: 500,
+    value: '50%',
     duration: 500
   })
 });
 
 function anime(selector, option) {
   const startTime = performance.now();
-  const currentValue = parseInt(getComputedStyle(selector)[option.prop]);
-  if (option.value === currentValue) return;
-  if (option.value > currentValue) requestAnimationFrame(plus);
-  if (option.value < currentValue) requestAnimationFrame(minus);
+  let currentValue = parseInt(getComputedStyle(selector)[option.prop]);
 
-  function plus(time) {
+  const isString = typeof option.value;
+  if (isString === 'string') {
+    //기존의 currentValue값을 다시 전체 브라우저폭 대비 퍼센트값으로 변환해서 재할당
+    const winW = window.innerWidth;
+    currentValue = (currentValue / winW) * 100;
+    option.value = parseFloat(option.value);
+  }
+  (option.value !== currentValue) && requestAnimationFrame(run);
+
+  function run(time) {
     let timelast = time - startTime;
     let progress = timelast / option.duration;
 
     (progress < 0) && (progress = 0);
     (progress > 1) && (progress = 1);
     if (progress < 1) {
-      requestAnimationFrame(plus);
+      requestAnimationFrame(run);
     } else {
       option.callback && option.callback();
     }
-    let result = currentValue + ((option.value - currentValue) * progress)
-    selector.style[option.prop] = `${result}px`;
+    let result = currentValue + ((option.value - currentValue) * progress);
+
+    if (isString === 'string') selector.style[option.prop] = `${result}%`;
+    else selector.style[option.prop] = `${result}px`;
   }
 
-  function minus(time) {
-    let timelast = time - startTime;
-    let progress = timelast / option.duration;
-
-    (progress < 0) && (progress = 0);
-    (progress > 1) && (progress = 1);
-    if (progress < 1) {
-      requestAnimationFrame(plus);
-    } else {
-      option.callback && option.callback();
-    }
-    let result = currentValue - ((currentValue - option.value) * progress)
-    selector.style[option.prop] = `${result}px`;
-  }
 }
