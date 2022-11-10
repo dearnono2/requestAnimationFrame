af4fe2eef9f3e468fee4e31c26e908c5b51542fe
dearnono2 <dearnono2@gmail.com>
2022-11-10 15:07:55 +0900

    Anime 클래스 완성

--- anime.js
+++ anime.js
@@ -1,60 +1,45 @@
-const btn = document.querySelector('button');
-const box = document.querySelector('#box');
+class Anime {
+  constructor(selector, option) {
+    this.selector = selector;
+    this.option = option;
+    this.startTime = performance.now();
+    this.currentValue = null;
 
-btn.addEventListener('click', () => {
-  anime(box, {
-    prop: 'margin-left',
-    value: 300,
-    duration: 500,
-    callback: () => {
-      anime(box, {
-        prop: 'margin-top',
-        value: 200,
-        duration: 500
-      })
+    this.option.prop === 'scroll'
+      ? this.currentValue = this.selector.scrollY
+      : this.currentValue = parseFloat(getComputedStyle(this.selector)[this.option.prop]);
+
+    this.isString = typeof this.option.value;
+    if (this.isString === 'string') {
+      const parentW = parseInt(getComputedStyle(this.selector.parentElement).width);
+      const parentH = parseInt(getComputedStyle(this.selector.parentElement).height);
+      const x = ['margin-left', 'margin-right', 'left', 'right', 'width'];
+      const y = ['margin-top', 'margin-bottom', 'top', 'bottom', 'height'];
+      for (let cond of x) (this.option.prop === cond) && (this.currentValue = (this.currentValue / parentW) * 100);
+      for (let cond of y) (this.option.prop === cond) && (this.urrentValue = (this.currentValue / parentH) * 100);
+      this.option.value = parseFloat(this.option.value);
     }
-  })
-});
-
-
-function anime(selector, option) {
-  const startTime = performance.now();
-  let currentValue = null;
-
-  //만약 옵션의 속성명이 scroll이면 scrollY값 구하고
-  //그렇지 않으면 getComputedStyle 반환값 저장
-  option.prop === 'scroll'
-    ? currentValue = selector.scrollY
-    : currentValue = parseFloat(getComputedStyle(selector)[option.prop]);
-
-  const isString = typeof option.value;
-  if (isString === 'string') {
-    const parentW = parseInt(getComputedStyle(selector.parentElement).width);
-    const parentH = parseInt(getComputedStyle(selector.parentElement).height);
-    const x = ['margin-left', 'margin-right', 'left', 'right', 'width'];
-    const y = ['margin-top', 'margin-bottom', 'top', 'bottom', 'height'];
-    for (let cond of x) (option.prop === cond) && (currentValue = (currentValue / parentW) * 100);
-    for (let cond of y) (option.prop === cond) && (currentValue = (currentValue / parentH) * 100);
-    option.value = parseFloat(option.value);
+    (this.option.value !== this.currentValue)
+      //requestAnimationFrame에 콜백으로 같은 코드블록상에 있는 일반함수가 들어가면 time값이 자동으로 인수로 전달됨
+      //prototype에 담겨있는 메서드에서는 자동으로 인수값이 전달되지 않기 때문에
+      //wrapping함수로 패키징을 해서 직접 time 인수를 전달
+      && requestAnimationFrame((time) => this.run(time));
   }
-  (option.value !== currentValue) && requestAnimationFrame(run);
 
-  function run(time) {
-    let timelast = time - startTime;
-    let progress = timelast / option.duration;
+  run(time) {
+    let timelast = time - this.startTime;
+    let progress = timelast / this.option.duration;
 
     (progress < 0) && (progress = 0);
     (progress > 1) && (progress = 1);
     (progress < 1)
-      ? requestAnimationFrame(run)
-      : option.callback && setTimeout(() => option.callback(), 0);
+      ? requestAnimationFrame((time) => this.run(time))
+      : this.option.callback && setTimeout(() => this.option.callback(), 0);
+    let result = this.currentValue + ((this.option.value - this.currentValue) * progress);
 
-    let result = currentValue + ((option.value - currentValue) * progress);
-
-    if (isString === 'string') selector.style[option.prop] = `${result}%`;
-    else if (option.prop === 'opacity') selector.style[option.prop] = result;
-    else if (option.prop === 'scroll') window.scroll(0, result);
-    else selector.style[option.prop] = `${result}px`;
+    if (this.isString === 'string') this.selector.style[this.option.prop] = `${result}%`;
+    else if (this.option.prop === 'opacity') this.selector.style[this.option.prop] = result;
+    else if (this.option.prop === 'scroll') window.scroll(0, result);
+    else this.selector.style[this.option.prop] = `${result}px`;
   }
-
 }
