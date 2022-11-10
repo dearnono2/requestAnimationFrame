d60c0832c8d11f058f8c72f8284c92cb833c1ece
dearnono2 <dearnono2@gmail.com>
2022-11-10 14:17:57 +0900

    스크롤 모션 적용

--- anime.js
+++ anime.js
@@ -2,37 +2,36 @@ const btn = document.querySelector('button');
 const box = document.querySelector('#box');
 
 btn.addEventListener('click', () => {
-  anime(box, {
-    prop: 'top',
-    value: '50%',
-    duration: 500
+  anime(window, {
+    prop: 'scroll',
+    value: 4000,
+    duration: 1000
   })
 });
 
+/*
+  window.scrollY: 현재 스크롤된 거리값
+  window.scroll(x, y) : x,y축 스크롤 강제 이동
+*/
+
 function anime(selector, option) {
   const startTime = performance.now();
-  let currentValue = parseInt(getComputedStyle(selector)[option.prop]);
+  let currentValue = null;
+
+  //만약 옵션의 속성명이 scroll이면 scrollY값 구하고
+  //그렇지 않으면 getComputedStyle 반환값 저장
+  option.prop === 'scroll'
+    ? currentValue = selector.scrollY
+    : currentValue = parseFloat(getComputedStyle(selector)[option.prop]);
 
   const isString = typeof option.value;
   if (isString === 'string') {
+    const parentW = parseInt(getComputedStyle(selector.parentElement).width);
+    const parentH = parseInt(getComputedStyle(selector.parentElement).height);
     const x = ['margin-left', 'margin-right', 'left', 'right', 'width'];
     const y = ['margin-top', 'margin-bottom', 'top', 'bottom', 'height'];
-
-    //퍼센트로 동작할지도 모를 가로폭에 관련된 속성명을 반복을 돌며 조건문 설정
-    //해당 조건일때 해당 요소의 직계부모 넓이값을 기준으로 변환한 퍼센트 값을 currentValue에 담음
-    for (let cond of x) {
-      if (option.prop === cond) {
-        const parentW = parseInt(getComputedStyle(selector.parentElement).width);
-        currentValue = (currentValue / parentW) * 100;
-      }
-    }
-
-    for (let cond of y) {
-      if (option.prop === cond) {
-        const parentH = parseInt(getComputedStyle(selector.parentElement).height);
-        currentValue = (currentValue / parentH) * 100;
-      }
-    }
+    for (let cond of x) (option.prop === cond) && (currentValue = (currentValue / parentW) * 100);
+    for (let cond of y) (option.prop === cond) && (currentValue = (currentValue / parentH) * 100);
     option.value = parseFloat(option.value);
   }
   (option.value !== currentValue) && requestAnimationFrame(run);
@@ -51,6 +50,8 @@ function anime(selector, option) {
     let result = currentValue + ((option.value - currentValue) * progress);
 
     if (isString === 'string') selector.style[option.prop] = `${result}%`;
+    else if (option.prop === 'opacity') selector.style[option.prop] = result;
+    else if (option.prop === 'scroll') window.scroll(0, result);
     else selector.style[option.prop] = `${result}px`;
   }
 
